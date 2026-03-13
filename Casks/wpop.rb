bb5cae5e4575ef2e63c78a477b9b3da421bc417a
Emmanuel Canto <emmanuel-canto@hotmail.com>
2026-03-13 18:28:05 -0500

    primer commit

---
+++ Casks/wpop.rb
@@ -0,0 +1,12 @@
+cask "wpop" do
+  version "1.0"
+
+  url "https://github.com/TUUSUARIO/wpop/releases/download/v1.0/Wpop.dmg"
+  sha256 :no_check
+
+  name "Wpop"
+  desc "Floating lyrics window for Apple Music and Spotify"
+  homepage "https://github.com/TUUSUARIO/wpop"
+
+  app "Wpop.app"
+end
