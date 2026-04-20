6f69b1cad8808cc9822288ac46f8ae3c806a1e5e
Emmanuel Canto <emmanuel-canto@hotmail.com>
2026-04-20 11:34:43 -0500

    se sube la nueva version de wpop 2.0

--- Casks/wpop.rb
+++ Casks/wpop.rb
@@ -2,7 +2,7 @@ cask "wpop" do
   version "2.0"
 
   url "https://github.com/EmmanuelCanto/wpop/releases/download/beta2.0/Wpop.dmg"
-  sha256 :no_check
+  sha256 "e28d731d1c44660d3e08d83ca18d76e0a68e749974a20748a009b8d9cc37e8b4"
 
   name "Wpop"
   desc "Floating lyrics window for Apple Music and Spotify"
